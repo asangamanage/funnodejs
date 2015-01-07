4fc57f5dfa1a87d8a2d8558b332f9cb7eae20a6b
Asanga Manage <asanga.lk@gmail.com>
2015-01-08 00:57:38 +1100

    Refactoring templates

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var swig  = require('swig');
 var server = http.createServer ( function (req, res){
 	var url_parts = url.parse(req.url,true);
 	var returnUrl = url_parts.query.returnUrl;
-	body = swig.renderFile('./template.html', {
+	body = swig.renderFile('./views/template.html', {
 		    returnUrl: returnUrl
 	});
 	res.end(body);
